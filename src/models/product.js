967d0e63725251082c380b94401581cbe8d78481
Kaustubha17 <kaustubhashah@gmail.com>
2022-01-15 01:10:11 +0530

    commiting changes for min and max price

--- src/models/product.js
+++ src/models/product.js
@@ -5,11 +5,27 @@ function listProducts(data,cb){
     var values=[];
     if(data.categoryId){
 
-        sql+="WHERE CategoryID=?";
+        sql+="WHERE CategoryID=? ";
         values.push(data.categoryId);
 
-
+if(data.minPrice){
+    sql+="AND PRICE>=? ";
+    values.push(data.minPrice);
+}
+if(data.maxPrice){
+    sql+="AND PRICE<=?";
+    values.push(data.maxPrice);
+}
     }
+    else if(data.minPrice){
+        sql+="WHERE PRICE>=?";
+        values.push(data.minPrice);
+    }
+    else if(data.maxPrice){
+        sql+="WHERE PRICE<=?";
+        values.push(data.maxPrice);
+    }
+
     sqlConnection.executeQuery(sql, values,(err, result) =>cb(err, result))   
 }
 
